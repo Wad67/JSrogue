4b68080a8f7058c7c0cfb530452a4a4f537ba0f0
Wayde <Wad67@live.com.au>
2015-10-20 17:36:01 +1000

    Update game.js

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ if (!ROT.isSupported()) {
 var display = new ROT.Display({width:80, height:80});
 var container = display.getContainer();
 document.body.appendChild(container);
-display.drawText(2, i, colors + "Hello, world!");
+display.drawText("Hello, world!");
 var foreground, background, colors;
 //for (var i = 0; i < 15; i++) {
     // Calculate the foreground color, getting progressively darker
